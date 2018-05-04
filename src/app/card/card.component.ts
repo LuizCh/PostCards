61a4fdc8538579e27a44c5a4ec473c4c1a36d3fa
Luis Felipe Chica <lchica@lchicamac.local>
2018-05-04 13:59:09 -0500

    delete comments

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,11 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
 	styleUrls: [ './card.component.scss' ]
 })
 export class CardComponent implements OnInit {
-	// userId: string;
-	// id: string;
-	// title: string;
-	// body: string;
-
 	@Input() post;
 
 	constructor() {}
